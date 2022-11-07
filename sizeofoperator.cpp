3317677ed81e621e10cc7e547b7feb0082bd20ea
pranjal300799 <pranjalkumarkunik@gmail.com>
2022-11-08 01:35:30 +0530

    Show the use of sizeof operator

---
+++ sizeofoperator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+    cout<<"sizeof information"<<endl;
+    cout<<"sizeofchar"<<sizeof(char)<<endl;
+    cout<<"int"<<sizeof(int)<<endl;
+    cout<<"short"<<sizeof(short)<<endl;
+    cout<<"long"<<sizeof(long)<<endl;
+    cout<<"long long"<<sizeof(long long)<<endl;
+    cout<<"long double"<<sizeof(long double)<<endl;
+
+
+}
